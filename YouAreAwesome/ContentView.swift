58d67addc16fb3bbff67e49ec482ecf159451aed
Jimmy Lin <jmyjlin@gmail.com>
2025-10-08 01:44:26 -0700

    Update after lesson 1.9

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -11,6 +11,7 @@ struct ContentView: View {
     @State private var message = ""
     @State private var imageName = ""
     @State private var imageNumber = 0
+    @State private var messageNumber = 0
     var body: some View {
         
         VStack {
@@ -26,15 +27,22 @@ struct ContentView: View {
                 .font(.largeTitle)
                 .fontWeight(.heavy)
                 .foregroundStyle(.red)
+                .multilineTextAlignment(.center)
             
             Spacer()
             
             Button("Show Message") {
-                let message1 = "You Are Awesome!"
-                let message2 = "You Are Great!"
-
-                message = (message == message1 ? message2 : message1)
+                let messages = ["You Are Amazing!",
+                                "You Are Terrific!",
+                                "You Are Cool!",
+                                "You Are Amazing Again!"]
                 
+                message = messages[messageNumber]
+                messageNumber += 1
+                
+                if messageNumber == messages.count {
+                    messageNumber = 0
+                }
                 
                 imageName = "image\(imageNumber)"
                 imageNumber += 1
