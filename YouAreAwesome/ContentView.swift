db912665d730e6e1a9c7a4bf538241d37424c49e
Jimmy Lin <jmyjlin@gmail.com>
2025-10-10 16:21:51 -0700

    Update after lesson 1.13

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -6,12 +6,17 @@
 //
 
 import SwiftUI
+import AVFAudio
 
 struct ContentView: View {
     @State private var message = ""
     @State private var imageName = ""
     @State private var lastMessageNumber = -1 // lastMessageNumber will never be -1
     @State private var lastImageNumber = -1 // lastImageNumber will never be -1
+    @State private var lastSoundNumber = -1 // lastSoundNumber will never be -1
+    @State private var audioPlayer: AVAudioPlayer!
+    let numberOfImages = 10 // images labeled iamge0 - image9
+    let numberOfSounds = 6 // sounds labeled sound0 - sound5
     var body: some View {
         VStack {
             Text(message)
@@ -22,6 +27,9 @@ struct ContentView: View {
                 .minimumScaleFactor(0.5)
                 .frame(height: 100)
                 .animation(.easeInOut(duration: 0.15), value: message)
+            
+            Spacer()
+            
             Image(imageName)
                 .resizable()
                 .scaledToFit()
@@ -48,11 +56,32 @@ struct ContentView: View {
                 
                 var imageNumber: Int
                 
-                repeat  {
-                    imageNumber = Int.random(in: 0...9)
+                repeat {
+                    imageNumber = Int.random(in: 0...(numberOfImages-1))
                 } while imageNumber == lastImageNumber
                 imageName = "image\(imageNumber)"
                 lastImageNumber = imageNumber
+                
+                var soundNumber: Int
+                
+                repeat {
+                    soundNumber = Int.random(in: 0...(numberOfSounds-1))
+                } while soundNumber == lastSoundNumber
+                let soundName = "sound\(soundNumber)"
+                lastSoundNumber = soundNumber
+                
+                guard let soundFile = NSDataAsset(name: soundName) else {
+                    print("😡 Could not read file named \(soundName)")
+                    return
+                }
+                
+                do {
+                    audioPlayer = try AVAudioPlayer(data: soundFile.data)
+                    audioPlayer.play()
+                } catch {
+                    print("😡 ERROR: \(error.localizedDescription) creating audioPlayer")
+                }
+                
             }
             .buttonStyle(.borderedProminent)
             .font(.title2)
